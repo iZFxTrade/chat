8292c52adc52c9bb98587ac1185af74190462e91
HTV <izfx.trade@gmail.com>
2023-03-16 23:30:02 +0700

    Firstx commit

--- README.md
+++ README.md
@@ -1,37 +1,22 @@
-### Chat App Development Front-End and Back-End using Flutter, SocketIo, and NodeJS.
 
-<a href="https://www.buymeacoffee.com/DevStack06" target="_blank"  align="center">
-    <p  align="center">
-        <br>
-   <img   src="https://cdn.buymeacoffee.com/buttons/default-orange.png" alt="Buy Me A Coffee" height="41" width="174">
-   </p>
-</a>
-<br>
-<img src= "https://github.com/DevStack06/images/blob/master/ChatImages/chatapp.png" >
-<br>
+# Socket.IO Chat
 
-### Playlist for Chat App Development series -:
+A simple chat demo for Socket.IO
 
-| Playlist Name                                               |     | YouTube Playlist Link                                                        |
-| ----------------------------------------------------------- | --- | ---------------------------------------------------------------------------- |
-| Chat App Development (Main Playlist)                        |     | [Link](https://youtube.com/playlist?list=PLtIU0BH0pkKovuEaNsrGE_Xd5Tz3m1zeC) |
-| Chat App Development (Only Front-End using Flutter)         |     | [Link](https://youtube.com/playlist?list=PLtIU0BH0pkKrgqaT-9JSrRRyetkVEkDn6) |
-| Chat Server Development (Only Back-End using Node/SocketIo) |     | [Link](https://youtube.com/playlist?list=PLtIU0BH0pkKqKm88PUSRWiKsZ50ztsITv) |
+## How to use
 
-<br>
+```
+$ npm i
+$ npm start
+```
 
-### **_If this tutorial helped you please give a star and also fork the repo, Thank you happy coding :)_**
+And point your browser to `http://localhost:3000`. Optionally, specify
+a port by supplying the `PORT` env variable.
 
-### To use this app follow below instructions-:
+## Features
 
-1. Clone this app using below syntax -:
-
-   > git clone https://github.com/DevStack06/Chat-Server.git
-
-2. After cloning install packages using below syntax -:
-   > npm install
-
-Above command will install all the neccery packges.
-
-3. Run the app on your mobile using below command -:
-   > npm run dev
+- Multiple users can join a chat room by each entering a unique username
+on website load.
+- Users can type chat messages to the chat room.
+- A notification is sent to all users when a user joins or leaves
+the chatroom.
--- index.js
+++ index.js
@@ -1,30 +1,76 @@
-const express = require("express");
-var http = require("http");
+// Setup basic express server
+const express = require('express');
 const app = express();
-const port = process.env.PORT || 5000;
-var server = http.createServer(app);
-var io = require("socket.io")(server);
+const path = require('path');
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+const port = process.env.PORT || 3000;
 
-//middlewre
-app.use(express.json());
-var clients = {};
-
-io.on("connection", (socket) => {
-  console.log("connetetd");
-  console.log(socket.id, "has joined");
-  socket.on("signin", (id) => {
-    console.log(id);
-    clients[id] = socket;
-    console.log(clients);
-  });
-  socket.on("message", (msg) => {
-    console.log(msg);
-    let targetId = msg.targetId;
-    if (clients[targetId]) clients[targetId].emit("message", msg);
-  });
+server.listen(port, () => {
+  console.log('Server listening at port %d', port);
 });
-app.route("/check").get((req,res) => {return res.json("App Good");});
 
-server.listen(port, "0.0.0.0", () => {
-  console.log("server started");
+// Routing
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Chatroom
+
+let numUsers = 0;
+
+io.on('connection', (socket) => {
+  let addedUser = false;
+
+  // when the client emits 'new message', this listens and executes
+  socket.on('new message', (data) => {
+    // we tell the client to execute 'new message'
+    socket.broadcast.emit('new message', {
+      username: socket.username,
+      message: data
+    });
+  });
+
+  // when the client emits 'add user', this listens and executes
+  socket.on('add user', (username) => {
+    if (addedUser) return;
+
+    // we store the username in the socket session for this client
+    socket.username = username;
+    ++numUsers;
+    addedUser = true;
+    socket.emit('login', {
+      numUsers: numUsers
+    });
+    // echo globally (all clients) that a person has connected
+    socket.broadcast.emit('user joined', {
+      username: socket.username,
+      numUsers: numUsers
+    });
+  });
+
+  // when the client emits 'typing', we broadcast it to others
+  socket.on('typing', () => {
+    socket.broadcast.emit('typing', {
+      username: socket.username
+    });
+  });
+
+  // when the client emits 'stop typing', we broadcast it to others
+  socket.on('stop typing', () => {
+    socket.broadcast.emit('stop typing', {
+      username: socket.username
+    });
+  });
+
+  // when the user disconnects.. perform this
+  socket.on('disconnect', () => {
+    if (addedUser) {
+      --numUsers;
+
+      // echo globally that this client has left
+      socket.broadcast.emit('user left', {
+        username: socket.username,
+        numUsers: numUsers
+      });
+    }
+  });
 });
