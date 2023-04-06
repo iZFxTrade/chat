b3077c7d9981953937c9a98d52a382b6ab11dee6
HTV <mgn63opt@MGN63s-MacBook-Air.local>
2023-04-06 14:34:27 +0700

    fix server

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ bot.command("ask", async (ctx) => {
         reply_to_message_id: ctx.message.message_id,
       }
     );
-  
+
     //  reply("Please ask anything after /ask");
   }
 });
@@ -50,7 +50,7 @@ bot.command("img", async (ctx) => {
   const text = ctx.message.text?.replace("/img", "")?.trim().toLowerCase();
 
   if (text) {
-   
+
     const res = await getImage(text);
 
     if (res) {
@@ -73,67 +73,197 @@ bot.command("img", async (ctx) => {
 });
 // khoi chay chat bot
 bot.launch();
+//================================================//
 
 // Setup basic express server
-const express = require('express');
-const app = express();
-const path = require('path');
-const server = require('http').createServer(app);
-const io = require('socket.io')(server);
+//const express = require('express');
+//const app = express();
+//const path = require('path');
+//const server = require('http').createServer(app);
+//const io = require('socket.io')(server);
+const io = require('socket.io')();
 const port = process.env.PORT || 3000;
-
+const rooms = io.sockets.adapter.rooms;
 const bodyParser = require('body-parser');
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+//app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({ extended: true }));
+io.listen(port);
 
-server.listen(port, () => {
+/* server.listen(port, () => {
   console.log('Server listening at port %d', port);
 
-});
+}); */
 
 // Routing
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Chatroom
-
+//app.use(express.static(path.join(__dirname, 'public')));
 let numUsers = 0;
-
 io.on('connection', (socket) => {
   let addedUser = false;
-  
+  const users = {}; // Lưu trữ các user đã kết nối với socket
+
+  // Chatroom
+  // Tạo namespace và lắng nghe kết nối đến các namespace tương ứng với từng room
+  const saleNamespace = io.of('/sale');
+  saleNamespace.on('connection', (socket) => {
+    console.log(`User ${socket.id} connected to room Sale`);
+
+    // Lưu thông tin user vào object users với key là socket.id và value là username
+    const { id } = socket;
+    const { username } = socket.handshake.query;
+    users[id] = username;
+
+    // In danh sách user trong room
+    const clients = Object.keys(saleNamespace.connected).map((socketId) => ({
+      id: socketId,
+      username: users[socketId],
+    }));
+    console.log(`Users in room Sale: `, clients);
+  });
+
+  const marketingNamespace = io.of('/marketing');
+  marketingNamespace.on('connection', (socket) => {
+    console.log(`User ${socket.id} connected to room Marketing`);
+
+    const { id } = socket;
+    const { username } = socket.handshake.query;
+    users[id] = username;
+
+    const clients = Object.keys(marketingNamespace.connected).map((socketId) => ({
+      id: socketId,
+      username: users[socketId],
+    }));
+    console.log(`Users in room Marketing: `, clients);
+  });
+
+  const cskhNamespace = io.of('/cskh');
+  cskhNamespace.on('connection', (socket) => {
+    console.log(`User ${socket.id} connected to room CSKH`);
+
+    const { id } = socket;
+    const { username } = socket.handshake.query;
+    users[id] = username;
+
+    const clients = Object.keys(cskhNamespace.connected).map((socketId) => ({
+      id: socketId,
+      username: users[socketId],
+    }));
+    console.log(`Users in room CSKH: `, clients);
+  });
+
+  const ktvNamespace = io.of('/ktv');
+  ktvNamespace.on('connection', (socket) => {
+    console.log(`User ${socket.id} connected to room KTV`);
+
+    const { id } = socket;
+    const { username } = socket.handshake.query;
+    users[id] = username;
+
+    const clients = Object.keys(ktvNamespace.connected).map((socketId) => ({
+      id: socketId,
+      username: users[socketId],
+    }));
+    console.log(`Users in room KTV: `, clients);
+  });
+
+  const modNamespace = io.of('/mod');
+  modNamespace.on('connection', (socket) => {
+    console.log(`User ${socket.id} connected to room MOD`);
+
+    const { id } = socket;
+    const { username } = socket.handshake.query;
+    users[id] = username;
+
+    const clients = Object.keys(modNamespace.connected).map((socketId) => ({
+      id: socketId,
+      username: users[socketId],
+    }));
+    console.log(`Users in room MOD: `, clients);
+  });
+
   // when the client emits 'new message', this listens and executes
   socket.on('new message', (data) => {
     //console.log('Received message %s: %s', username , data);
     const userId = data.to;
-    // we tell the client to execute 'new message'
-    socket.broadcast.emit('new message', {
-      username: socket.username,
-      message: data,
-      
-    });
-    
-  });
-// Lắng nghe sự kiện khi một client kết nối đến server
-io.on('connection', (socket) => {
-  console.log('A user connected.');
+    var User;
 
-  // Lắng nghe sự kiện chat message từ client
-  socket.on('chat message', (msg) => {
-    console.log('Received message:', msg);
-
-    // Gửi tin nhắn cho một user duy nhất với socketId tương ứng
-    const userId = msg.to;
-    const socketId = getUserSocketId(userId); // Lấy socketId của user từ id
-    if (socketId) {
-      socket.to(socketId).emit('chat message', msg);
-      console.log('Sent message to user', userId);
+    if (typeof data === 'string') {
+      User = socket.console;
+      var msg = data;
+    } else if (typeof data === 'object' && data !== null && typeof data.username === 'string') {
+      User = socket.console.username;
+      var msg = data.message;
     } else {
-      console.log('User not found:', userId);
+      console.error('Invalid data type received:', data);
+      return;
     }
-  });
+    // gửi nội dung đến tất cả các user đang kết nối
+    if (data.username === "All" || typeof data === 'string') {
+      socket.broadcast.emit('new message', {
+        username: User,
+        message: msg,
+      });
+      console.log(rooms);
+      console.log('New message to All from user', User, ': ', msg);
+    }
+    if (data.username === "Sale") {
+      socket.join("sale");
+      io.to("sale").emit("new message", "Hello from CSKH room!");
+    }
+    if (data.username === "Marketing") {
+      socket.join("marketing");
+      io.to("marketing").emit("new message", "Hello from CSKH room!");
+    }
+    if (data.username === "CSKH") {
+      socket.join("cskh");
+      io.to("cskh").emit("new message", "Hello from CSKH room!");
+    }
+    if (data.username === "KTV") {
+      socket.join("ktv");
+      io.to("ktv").emit("new message", "Hello from CSKH room!");
+    }
+    if (data.username === "MOD") {
+      socket.join("mod");
+      io.to("mod").emit("new message", "Hello from CSKH room!");
+    }
+    else {
+      // gửi nội dung đến user đó
+      socket.to(data.username).emit('new message', {
+        username: User,
+        message: msg,
+      });
+      console.log(rooms);
+      console.log('New message', User, 'to', data.username, ': ', msg);
+    }
+    /* socket.broadcast.emit('new message', {
+      username: User,
+      message: msg,
+    });
+    console.log('New message to All from user', User, ': ', msg);
+ */
+    // Gửi tin nhắn cho một user duy nhất với socketId tương ứng dung 
 
-  // ...
-});
+  });
+  // Lắng nghe sự kiện khi một client kết nối đến server
+  io.on('connection', (socket) => {
+    console.log('A user connected: ', socket.id);
+
+    // Lắng nghe sự kiện chat message từ client
+    socket.on('chat message', (msg) => {
+      console.log('Received message:', msg);
+
+      // Gửi tin nhắn cho một user duy nhất với socketId tương ứng
+      const userId = msg.to;
+      const socketId = getUserSocketId(userId); // Lấy socketId của user từ id
+      if (socketId) {
+        socket.to(socketId).emit('chat message', msg);
+        console.log('Sent message to user', userId);
+      } else {
+        console.log('User not found:', userId);
+      }
+    });
+
+    // ...
+  });
 
 
   // when the client emits 'add user', this listens and executes
@@ -141,7 +271,9 @@ io.on('connection', (socket) => {
     if (addedUser) return;
 
     // we store the username in the socket session for this client
-    socket.username = username;
+    socket.console = username;
+
+    console.log('Adding user ', username, 'ID: ', socket.id);
     ++numUsers;
     addedUser = true;
     socket.emit('login', {
@@ -160,6 +292,11 @@ io.on('connection', (socket) => {
       username: socket.username
     });
   });
+
+  // print msg form client
+  socket.on('msg_from_client', (msg) => {
+    console.log('Received message:', msg, ' ID: ', socket.id);
+  })
 
   // when the client emits 'stop typing', we broadcast it to others
   socket.on('stop typing', () => {
@@ -182,8 +319,10 @@ io.on('connection', (socket) => {
   });
 });
 
+//===========================================//
+/* 
 // Add webhook endpoint
- app.post('/webhook/:username', (req, res) => {
+app.post('/webhook/:username', (req, res) => {
   const { username } = req.params;
   const data = req.body;
   const msg = JSON.stringify(data, null, 2);
@@ -198,8 +337,8 @@ io.on('connection', (socket) => {
       text += `${k}: ${data[key]}\n`; // Thêm cặp giá trị vào chuỗi văn bản, với mỗi giá trị nằm trên một dòng mới
     }
   }
- 
-  
+
+
   console.log('Noi dung Webhook: %s', data);
   io.emit('new message', {
     username,
@@ -210,3 +349,4 @@ io.on('connection', (socket) => {
   res.status(200).send(`Webhook received for ${username}  : ${JSON.stringify(data)}`);
 });
 
+ */
